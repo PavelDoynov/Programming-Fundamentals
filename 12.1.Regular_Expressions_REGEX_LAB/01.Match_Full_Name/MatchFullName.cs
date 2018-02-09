8b371f34dd9949a7c45633f4933e6f695d6825a8
Pavel Doynov <paveldoynov@Pavels-MacBook-Pro.local>
2018-02-09 18:52:56 +0200

    Programming Fundamentals course with C#

---
+++ 12.1.Regular_Expressions_REGEX_LAB/01.Match_Full_Name/MatchFullName.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * 01. Match Full Name
+ * 
+ * Write a C# Program to match full names from a list of names and print them on the console.
+ * 
+ * Example
+ * Input:
+ * Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan    Ivanov
+ * 
+ * Output:
+ * Ivan Ivanov Test Testov
+ * 
+ * https://github.com/PavelDoynov
+ */
+
+
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _01.Match_Full_Name
+{
+    class Program
+    {
+        static void Main()
+        {
+            string pattern = @"\b([A-Z][a-z]+ [A-Z][a-z]+)\b";
+
+            string input = Console.ReadLine();
+
+            List<string> result = new List<string>();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection match = Regex.Matches(input, pattern);
+
+            foreach (Match name in match)
+            {
+                result.Add(name.ToString());
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
